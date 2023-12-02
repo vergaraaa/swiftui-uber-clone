820d17ac058f9821447492fe8159b223a8933c28
Edgar Vergara <edgarvm13@hotmail.com>
2023-12-02 13:18:39 +0100

    Initial Commit

---
+++ Uber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uber
+//
+//  Created by Edgar Ernesto Vergara Montiel on 02/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Edgar Ernesto Vergara Montiel on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
